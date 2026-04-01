4e18a19ecea76085d6d2b1cb3e0732727d119062
Wang <jiahaowang2007@163.com>
2026-04-01 19:33:53 +0800

    加入BMI088

---
+++ LogicCode/inc/crt_imu.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "bmi088_mm.h"
+
+class IMU
+{
+public:
+    IMU() = default;
+    ~IMU() = default;
+
+public:
+    bmi08_sensor_data m_accelData;
+    bmi08_dev m_bmi08;
+    int8_t m_rslt;
+    float m_accelx;
+    float m_accely;
+    float m_accelz;
+
+public:
+    void imuInit();
+    static void init_bmi08(struct bmi08_dev *bmi08dev);
+    static float lsb_to_mps2(int16_t val, float g_range, uint8_t bit_width);
+    static float lsb_to_dps(int16_t val, float dps, uint8_t bit_width);
+    void imuLoop();
+
+private:
+};
